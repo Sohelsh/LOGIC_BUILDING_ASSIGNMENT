469d6cef5df78ecce6973a7c8cee9b732bffa3bb
Sohelsh <sohelshaikhadam@gmail.com>
2022-10-20 23:39:14 +0530

    Added File

---
+++ Assignment_16/01.Accept Number Of Rows And Number Of Columns From User And Display Below Pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0,j = 0;
+    char Ch = '\0';
+
+    for(i = 1;i <= iRow;i++)
+    {
+        for(Ch = 'A',j = 1;j <= iCol;j++,Ch++)
+        {
+            printf(" %c ",Ch);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0,iValue2 = 0;
+
+    printf("\n Enter Number Of Rows And Colums = ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
