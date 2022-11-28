431b46212c7f36a5d54a3205d3c2b7d2fa966410
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-28 11:56:49 +0545

    Assignment No 22

---
+++ Assignment_22/04.Accept N Numbers From User And Return Frequency Of 11 Form It.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Frequency(int Arr[],int iLength)
+{
+    int i = 0,Cnt = 0;
+
+    for(i = 0;i < iLength; i++)
+    {
+        if(Arr[i] == 11)
+        {
+            Cnt++;
+        }
+
+    }
+    return Cnt;
+}
+int main()
+{
+    int iSize = 0,iCnt = 0,iRet = 0;
+    int *p = NULL;
+
+    printf("\n Enter Number Of Elements = ");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("\n Unable To Allocate Memory ");
+        return -1;
+    }
+
+    printf("\n Enter %d Elements ",iSize);
+
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        printf("\n Enter Element %d : ",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Frequency(p,iSize);
+
+    printf("\n Result Is %d",iRet);
+
+    free(p);
+
+    getch();
+    return 0;
+}
+
